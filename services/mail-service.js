af09a8cae67293e56cd7248b64d7667d377740ac
Lior Orfelli <liorfelli1295@gmail.com>
2020-08-26 15:03:32 +0300

    mail-app-structure

---
+++ services/mail-service.js
@@ -0,0 +1,13 @@
+
+const mails = [
+        {id:m101, subject: 'Wassap?', body: 'Pick up!', isRead: false, markedAsRead:false, sentAt : 1551133930594},
+        {id:m102, subject: 'Hey?', body: 'HELLO!', isRead: false, markedAsRead:false, sentAt : 1551133930594+200},
+        {id:m103, subject: 'dsd?', body: 'Hey!', isRead: false, markedAsRead:false, sentAt : 1551133930594+3000}
+]
+
+ function getEmails(){
+    return Promise.resolve()
+}
+
+
+
